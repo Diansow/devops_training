54978e03f2318719406112c82e3c5769bd470be1
Diansow <sowdian@gmail.com>
2023-11-02 20:30:48 +0000

    Ajout de nexus, prometheus, ...

--- applications/python/main.py
+++ applications/python/main.py
@@ -1,36 +1,36 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
 
-@app.route('/home')
-def index():
-    return 'home page'
+# @app.route('/home')
+# def index():
+#     return 'home page'
 
-#@app.route('/hi')
-#def hello():
- #   return 'Hello, World'
+# #@app.route('/hi')
+# #def hello():
+#  #   return 'Hello, World'
 
-from markupsafe import escape
+# from markupsafe import escape
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'Hello, {escape(username)}'
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return f'Hello, {escape(username)}'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return f'Post {post_id}'
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#     # show the subpath after /path/
+#     return f'Subpath {escape(subpath)}'
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+# @app.route("/<name>")
+# def hello(name):
+#     return f"Hello, {escape(name)}!"
--- applications/python/saguitorde.py
+++ applications/python/saguitorde.py
@@ -1,12 +1,34 @@
-from flask import render_template, Flask
+from flask import Response, Flask
+#import flask_monitoringdashboard as dashboard
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('tata.html', name=name)
+#export DEBUG_METRICS=false
+#dashboard.bind(app)
+metrics = PrometheusMetrics(app=app)
+app.debug = True
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def index():
+    return "WELCOME!"
+
+
+@app.route("/v1/greetings")
+def greetings():
+    return "Hello World!"
+
+
+@app.route("/v1/test")
+def greetingsTest():
+    return "Hello test!"
+
+
+@app.route("/v1/prod")
+def greetingsProd():
+    return "Hello prod!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
